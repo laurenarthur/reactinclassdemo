8837cccb447c5ed2e163ed3288118b77ba6834f5
laurenarthur <arthur_lauren@columbusstate.edu>
2020-11-11 18:00:55 +0000

    to-do list

---
+++ reactdemo/src/TaskItems.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class TaskItems extends Component{
+    render(){
+        return (
+            <div>
+                Individual Tasks  
+            </div>
+        )
+    }
+}
+
+export default TaskItems;
