192a331907b83ebc7a07a7a1a6ea9f74834c5bb4
laurenarthur <arthur_lauren@columbusstate.edu>
2020-11-23 20:06:42 +0000

    error

--- reactdemo/src/TaskList.jsx
+++ reactdemo/src/TaskList.jsx
@@ -2,16 +2,18 @@ import React, {Component} from 'react';
 import TaskItems from './TaskItems';
 
 class TaskList extends Component{
+   
     render(){
-        const {title, buttonText, tasks, buttonFunction} = this.props;
+        const {title, buttonText, tasks, buttonFunction} =this.props;
         const taskItems = tasks.map((task) => <TaskItems tasks={task} key={task} buttonFunction={buttonFunction}/>);
         return (
             <div>
                 {title}
                 {taskItems}
+                {/* <button onClick={() => buttonFunction}>{buttonText}</button> */}
             </div>
         )
     }
 }
 
-export default TaskList;
+export default TaskList
